d5a96342c9793e06c0d90357fb2a98be1824351b
root <root@DBA-Dev-SVN.(none)>
2013-06-08 15:27:17 +0800

    for test

---
+++ t.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#_*_coding:utf-8 _*_
+
+__filename__ = 't.py'
+__author__ = 'CuiJing'
+__email__ = 'mr.cuijing@gmail.com'
+__created__ = '2013-06-08 15:17'
+__description__ = ''
+
+print 'test'
+
